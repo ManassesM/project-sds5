fc525f2cb76c9aa8d05a3a8f7673b943666ff564
ManassesM <manamiui2001@gmail.com>
2021-11-06 13:26:21 -0300

    Home, navigation

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,7 +1,26 @@
+import Footer from 'components/Footer'
+import NavBar from 'components/NavBar'
+import { Link } from 'react-router-dom'
+
 const Home = () => {
 	return (
-    <h1>Homepage</h1>
-  )
+		<>
+			<NavBar />
+			<div className='container'>
+				<h1 className='display-4'>DSVendas</h1>
+				<p className='lead'>
+					Analyse the development of your sales from different perspectives
+				</p>
+				<hr />
+				<p>
+					This application consists of displaying a dashboard from sources
+					provided by a backend built with Spring Boot.
+				</p>
+				<Link to='/dashboard' className="btn btn-primary btn-lg">Access dashboard</Link>
+			</div>
+			<Footer />
+		</>
+	)
 }
 
 export default Home
