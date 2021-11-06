d4013148806ecc2a47d878e28730cadc90c34fac
ManassesM <manamiui2001@gmail.com>
2021-11-06 14:45:21 -0300

    Pagination

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -1,10 +1,12 @@
 import axios from 'axios'
+import Pagination from 'components/Pagination'
 import { useEffect, useState } from 'react'
 import { SalePage } from 'types/sale'
 import { formatLocalDate } from 'utils/format'
 import { BASE_URL } from 'utils/requests'
 
 const DataTable = () => {
+	const [activePage, setActivePage] = useState(0)
 	const [page, setPage] = useState<SalePage>({
 		first: true,
 		last: true,
@@ -14,36 +16,43 @@ const DataTable = () => {
 	})
 
 	useEffect(() => {
-		axios.get(`${BASE_URL}/sales?page=0&size=20&sort=date,desc`).then((res) => {
+		axios.get(`${BASE_URL}/sales?page=${activePage}&size=20&sort=date,desc`).then((res) => {
 			setPage(res.data)
 		})
-	}, [])
+	}, [activePage])
+
+  const changePage = (idx: number) => {
+    setActivePage(idx)
+  }
 
 	return (
-		<div className='table-responsive'>
-			<table className='table table-striped table-sm'>
-				<thead>
-					<tr>
-						<th>Date</th>
-						<th>Seller</th>
-						<th>Visited Clients</th>
-						<th>Deals</th>
-						<th>Amount</th>
-					</tr>
-				</thead>
-				<tbody>
-					{page.content?.map((item) => (
-						<tr key={item.id}>
-							<td>{formatLocalDate(item.date, 'MM/dd/yyyy')}</td>
-							<td>{item.seller.name}</td>
-							<td>{item.visited}</td>
-							<td>{item.deals}</td>
-							<td>{item.amount}</td>
+		<>
+			<Pagination page={page} onPageChange={changePage} />
+			<div className='table-responsive'>
+				<table className='table table-striped table-sm'>
+					<thead>
+						<tr>
+							<th>Date</th>
+							<th>Seller</th>
+							<th>Visited Clients</th>
+							<th>Deals</th>
+							<th>Amount</th>
 						</tr>
-					))}
-				</tbody>
-			</table>
-		</div>
+					</thead>
+					<tbody>
+						{page.content?.map((item) => (
+							<tr key={item.id}>
+								<td>{formatLocalDate(item.date, 'MM/dd/yyyy')}</td>
+								<td>{item.seller.name}</td>
+								<td>{item.visited}</td>
+								<td>{item.deals}</td>
+								<td>{item.amount}</td>
+							</tr>
+						))}
+					</tbody>
+				</table>
+			</div>
+		</>
 	)
 }
 
